09171acfefbd2a887feba14e789c401d30279247
natalieburkhard <97681247+natalieburkhard@users.noreply.github.com>
2022-04-20 13:20:39 -0400

    Work from week 6
    Attempting BLAST

---
+++ Week 6/attempt2.R
@@ -0,0 +1,9 @@
+
+install.packages("taxonomizr")
+install.packages("rBLAST")
+install.packages("devtools")
+devtools::install_github("mhahsler/rBLAST")
+
+library(taxonomizr)
+library(rBLAST)
+library(ggplot2)
